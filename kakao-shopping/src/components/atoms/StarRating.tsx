36737a073ccf2b99493f7c84e06baaa154f3483e
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-08-02 15:15:33 +0900

    docs: 리드미 수정

--- kakao-shopping/src/components/atoms/StarRating.tsx
+++ kakao-shopping/src/components/atoms/StarRating.tsx
@@ -1,12 +1,6 @@
-import { useState, useEffect, useRef } from 'react';
 const StarRating = ({starCount}) => {
   //return starts as much as starCount
   const stars = [];
-  const [count, setCount] = useState(0);
-  const countRef = useRef(0);
-  const observer = useRef(4);
-  let thiss = 10;
-  console.log(count, observer.current, thiss);
 
   for (let i = 0; i < starCount; i++) {
     stars.push(<img src="/assets/blueStar.png" alt="star" className="w-4 h-4" />);
@@ -15,11 +9,7 @@ const StarRating = ({starCount}) => {
   return (
     <div className="flex">
     {stars.map((star, index) => (
-      <div onClick={() => {
-        setCount(prev => prev + 1)
-        observer.current = count;
-        thiss = count; 
-      }} key={index}>{star}</div>
+      <div key={index}>{star}</div>
     ))}
     </div>
   )
